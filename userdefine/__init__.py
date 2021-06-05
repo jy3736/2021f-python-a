37503e05ef6c491869e97e351a0b18ab1e8a7aaf
jy3736 <jyang@stust.edu.tw>
2021-06-06 01:33:43 +0800

    Update __init__.py

--- userdefine/__init__.py
+++ userdefine/__init__.py
@@ -8,4 +8,4 @@ def exists(): # the name of the check
 @check50.check(exists) # only run this check if the exists check has passed
 def prints_hello():
  """prints "hello, world.\\n" """
- check50.run("python3 hello.py").stdout("[Hh]ello, world.\n", regex=True).exit(0)
+ check50.run("python3 hello.py").stdout("[Hh]ello, world\.\n", regex=True).exit(0)
