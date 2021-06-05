39350d803bc29d85caa7f52e7a2ab524f9adb67c
jy3736 <jyang@stust.edu.tw>
2021-06-05 13:32:06 +0800

    Update __init__.py

--- week02/__init__.py
+++ week02/__init__.py
@@ -3,4 +3,4 @@ import check50
 @check50.check()
 def w02_hw01():
     """w02-hw01"""
-    check50.run("python hw01.py").stdin("4", prompt=False).stdout("*\n**\n***\n****\n", regex=False).exit(0)
+    check50.run("python hw01.py").stdin("3", prompt=False).stdout("*\n**\n***\n****\n", regex=False).exit(0)
