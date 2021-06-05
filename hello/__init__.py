000e5927d4cc85000c3cd333e5af824ebccf8207
jy3736 <jyang@stust.edu.tw>
2021-06-05 22:12:06 +0800

    Update __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -9,7 +9,7 @@ def exists():
 @check50.check(exists)
 def compiles():
     """hello.c compiles"""
-    check50.c.compile("hello.c", lcs50=True)
+    check50.c.compile("hello.c", cc="gcc", lcs50=True)
 
 @check50.check(compiles)
 def emma():
