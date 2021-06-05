86fba32d5ddb8d40ff5bd3acc80ad57575c8d3a9
jy3736 <jyang@stust.edu.tw>
2021-06-05 16:30:47 +0800

    Update check.py

--- week03/check.py
+++ week03/check.py
@@ -1,6 +1,11 @@
 import check50
 
 @check50.check()
-def w02_hw01():
+def w02_hw01_a():
     """w02-hw01"""
-    check50.run("python hw01.py").stdin("3", prompt=False).stdout("*\n**\n***\n****\n", regex=False).exit(0)
+    check50.run("python hw01.py").stdin("4", prompt=False).stdout("*\n**\n***\n****\n", regex=False).exit(0)
+    
+@check50.check()
+def w02_hw01_b():
+    """w02-hw01"""
+    check50.run("python hw01.py").stdin("2", prompt=False).stdout("*\n**\n", regex=False).exit(0)
