1bd844c3a95461c7f9187f72279db3c14f9845bd
jy3736 <jyang@stust.edu.tw>
2021-06-05 14:34:15 +0800

    Update __init__.py

--- week01/__init__.py
+++ week01/__init__.py
@@ -1,16 +1,6 @@
 import check50
 
 @check50.check()
-def w01_hw01_basic_check():
-    """w01-hw01 basic check"""
-    check50.run("python hw01.py").stdout("Hello Python World!", regex=False).exit(0)
-
-@check50.check()
-def w01_hw02_1_test_case():
-    """w01-hw02 1 test case"""
-    check50.run("python hw02.py").stdin("1 2", prompt=False).stdout("1 2 3", regex=False).exit(0)
-
-@check50.check()
-def w01_hw02_3_test_cases():
-    """w01-hw02 3 test cases"""
-    check50.run("python hw02.py").stdin("1 2 3 4", prompt=False).stdout("1 2 3 4 10", regex=False).exit(0)
+def hello_world():
+    """hello world"""
+    check50.run("python3 hw01.py").stdout("Hello, world!", regex=False).exit(0)
